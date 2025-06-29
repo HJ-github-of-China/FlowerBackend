c9c5c106935b5bb60e93220c8afd34803570a397
zhiyun <2669807847@qq.com>
2025-06-29 20:48:55 +0800

    feat(api): 添加 OpenAPI (Swagger) 支持
    - 在 application.yml 中添加 springdoc-openapi配置
    - 新增 LineChartsController 类并添加 API 文档注解
    - 在 pom.xml 中添加 knife4j-openapi3 依赖
    - 新增 OpenApiConfig配置类（待完善）

---
+++ Topbiz/src/main/java/com/flower/config/OpenApiConfig.java
@@ -0,0 +1,6 @@
+package com.flower.config;
+
+//TODO Knife4j 配置类待完善
+public class OpenApiConfig {
+
+}
--- Topbiz/src/main/java/com/flower/controller/LineChartsController.java
+++ Topbiz/src/main/java/com/flower/controller/LineChartsController.java
@@ -2,14 +2,19 @@ package com.flower.controller;
 
 import com.flower.service.LineChartsService;
 import com.flower.vo.LineChartSVO;
+import com.github.xiaoymin.knife4j.annotations.ApiSupport;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name = "模型监控", description = "模型训练结果展示")
 @RestController
-@RequestMapping("/api/train")
+@RequestMapping("api/monitor")
 @Slf4j
 public class LineChartsController {
         @Autowired
@@ -20,6 +25,8 @@ public class LineChartsController {
          * @param pattern 匹配模式
          * @return 训练集or测试集
          */
+
+        @Operation(summary = "展示折线图", description = "通过Echarts展示折线图")
         @GetMapping("/line")
         public LineChartSVO getTrainData(String pattern) {
 
